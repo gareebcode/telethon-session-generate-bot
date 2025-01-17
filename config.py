9e2ba43d4284e15493b2d8f4ddf202cd60462290
Anuj Tiwari <meanddevil666@outlook.com>
2025-01-17 16:16:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # Telegram API credentials
-API_ID = 23037009  # Replace with your actual Telegram API ID
-API_HASH = "9a214f65832749215b9532cdb3df73b9"  # Replace with your actual Telegram API Hash
+API_ID = 9  # Replace with your actual Telegram API ID
+API_HASH = ""  # Replace with your actual Telegram API Hash
 
 # Bot token obtained from BotFather
-BOT_TOKEN = "7326914757:AAGukO4Yx1LPV9eTZ69oIun1y-SlMDmo7yI"  # Replace with your actual Bot Token
+BOT_TOKEN = ""  # Replace with your actual Bot Token
